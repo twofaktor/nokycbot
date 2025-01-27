fcbb84c33e300cc2ed1786b13c5dfe74dd7f999c
⚡️2FakTor⚡️ <twofaktor@protonmail.com>
2025-01-27 11:48:18 +0100

    Update bisq.py

--- exchanges/bisq.py
+++ exchanges/bisq.py
@@ -19,7 +19,7 @@ class Bisq:
         # refprice = int
         # tor = 1 or 0
 
-        bisqBaseUrlTor = 'http://bisqmktse2cabavbr2xjq7xw3h6g5ottemo5rolfcwt6aly6tp5fdryd.onion'
+        bisqBaseUrlTor = 'http://bisqmktwih46lbda65e27uefnn2nznnk5oygdwyqcdrpo7yy7i5qd7ad.onion'
 
         bisqApi = f"{bisqBaseUrlTor}/api/offers?market=btc_{fiat.upper()}&direction={direction.upper()}"
         try:
@@ -59,7 +59,7 @@ class Bisq:
         return alloffers
 
     def getFiatPrice(fiat, session):
-        bisqApi = 'http://emzypricpidesmyqg2hc6dkwitqzaxrqnpkdg3ae2wef5znncu2ambqd.onion/getAllMarketPrices'
+        bisqApi = 'http://ro7nv73awqs3ga2qtqeqawrjpbxwarsazznszvr6whv7tes5ehffopid.onion/getAllMarketPrices'
         try:
             f = session.get(bisqApi)
         except requests.exceptions.Timeout as e:
