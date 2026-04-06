85946de5eb4aa7f93fd49f627a66731328c01edc
⚡️2FakTor⚡️ <twofaktor@protonmail.com>
2026-04-06 21:22:32 +0200

    Set coordinators

--- exchanges/robosats.py
+++ exchanges/robosats.py
@@ -27,10 +27,11 @@ class Robosats:
     def getOffers(fiat, direction, session):
         robosatsTorAddresses = [
             'http://ngdk7ocdzmz5kzsysa3om6du7ycj2evxp2f2olfkyq37htx3gllwp2yd.onion',
-            #'http://mmhaqzuirth5rx7gl24d4773lknltjhik57k7ahec5iefktezv4b3uid.onion',
+            'http://alice7bqexhtnkiqhtgkuwgtzzfkishw23ac4sfwpznrwlmnipxlomyd.onion',
             'http://4t4jxmivv6uqej6xzx2jx3fxh75gtt65v3szjoqmc4ugdlhipzdat6yd.onion',
             'http://otmoonrndnrddqdlhu6b36heunmbyw3cgvadqo2oqeau3656wfv7fwad.onion',
             'http://librebazovfmmkyi2jekraxsuso3mh622avuuzqpejixdl5dhuhb4tid.onion',
+            'http://ammannjgzybw4qm2odmci7xgolh5grzijidacjxose5tthm375dcopad.onion',
             'http://dqmmejfmtlve7d4ccohk4usriifdtci6xk4wv7igxn2fyaduh25s6did.onion'
         ]
 
